c6f32cebd5d236dff97d25a45e902333bbdb9e00
Danillo <danillo.yamagush@gmail.com>
2026-04-16 12:04:52 -0300

    chore: add spring boot crm core service

---
+++ services/crm-core-service/crm/src/test/java/crm_core_service/CrmApplicationTests.java
@@ -0,0 +1,13 @@
+package crm_core_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
